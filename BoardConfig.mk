6c65ba32aa9873915e91ffd89b0e23b965c836dd
Rox-Ace2 <rooobertinhooo@gmail.com>
2013-09-09 15:04:11 +0200

    Board egl needs legacy_fb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -61,7 +61,7 @@ BOARD_CUSTOM_BOOTIMG_MK := device/samsung/codina/shbootimg.mk
 
 # Graphics
 BOARD_EGL_CFG := device/samsung/codina/configs/egl.cfg
-#BOARD_EGL_NEEDS_LEGACY_FB := true
+BOARD_EGL_NEEDS_LEGACY_FB := true
 USE_OPENGL_RENDERER := true
 
 # Bluetooth
