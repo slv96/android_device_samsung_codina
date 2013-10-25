6e8838e2404f4bdfe7833bab55e74c4a7571de29
Rox-Ace2 <rooobertinhooo@gmail.com>
2013-10-25 15:30:36 +0200

    Clean recovery.rc

--- codina.mk
+++ codina.mk
@@ -27,6 +27,7 @@ PRODUCT_PACKAGES += \
 # Init files
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/fstab.samsungcodina:root/fstab.samsungcodina \
+    $(LOCAL_PATH)/rootdir/init.recovery.samsungcodina.rc:root/init.recovery.samsungcodina.rc \
     $(LOCAL_PATH)/rootdir/init.samsungcodina.rc:root/init.samsungcodina.rc \
     $(LOCAL_PATH)/rootdir/init.samsungcodina.usb.rc:root/init.samsungcodina.usb.rc \
     $(LOCAL_PATH)/rootdir/ueventd.samsungcodina.rc:root/ueventd.samsungcodina.rc
