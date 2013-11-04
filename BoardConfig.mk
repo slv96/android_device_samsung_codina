376b22b44e1849498099540e2ce134f32075b599
Robert Rozic <rooobertinhooo@gmail.com>
2013-11-04 16:48:57 +0100

    SELinux defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,7 @@ TARGET_OTA_ASSERT_DEVICE := codina,i8160,GT-I8160
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/codina
 TARGET_KERNEL_CONFIG := cyanogenmod_i8160_defconfig
+TARGET_KERNEL_SELINUX_CONFIG := codinaselinux_defconfig
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/codina/bluetooth
