55983e5678a5762a13488501dc9145c2fbc0ece0
asdfzz <moog87@gmail.com>
2013-12-02 11:32:17 +0100

    Remove cm stuff

--- omni_codina.mk
+++ omni_codina.mk
@@ -11,9 +11,6 @@ PRODUCT_RELEASE_NAME := GT-I8160
 TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 480
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/samsung/codina/full_codina.mk)
 
