11dcf9ca71b88718915acec46f3d510684eca46d
Robert Rozic <rooobertinhooo@gmail.com>
2014-03-04 14:18:48 +0100

    Delete unneded files and lines

--- codina.mk
+++ codina.mk
@@ -29,10 +29,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/adm.sqlite-u8500:system/etc/adm.sqlite-u8500
 
-# Dbus
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/dbus.conf:system/etc/dbus.conf
-
 # Gps
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf
